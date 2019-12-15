057c6d75184c0996386bb84380073f408d6b02ab
Kelly Selden <kellyselden@gmail.com>
2019-12-15 12:15:18 +0000

    remove custom hint guessing, use built in

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 const { EOL } = require('os');
 const ThreeWayMerger = require('three-way-merger');
 const rfc6902 = require('rfc6902-ordered');
-const { Range } = require('semver');
 
 const dependencyKeys = [
   'dependencies',
@@ -23,49 +22,10 @@ function sortObjectKeys(obj) {
   }, {});
 }
 
-// matches "^1.0.0" but not "^1.0.0 || ^3.0.0" or "1.0"
-function hasSimpleHint(rangeString) {
-  let range = new Range(rangeString);
-  let isSimpleRange = range.set.length === 1 && range.set[0].length === 2;
-  return isSimpleRange && !!getHint(rangeString);
-}
-
-// matches "1.0.0" and "1.0" but not "^1.0.0"
-function hasNoHint(rangeString) {
-  let range = new Range(rangeString);
-  let isPinned = range.set.length === 1 && range.set[0].length === 1;
-  return isPinned || !getHint(rangeString);
-}
-
-function getHint(rangeString) {
-  let matches = rangeString.match(/^ *[\^~]/);
-  return matches && matches[0];
-}
-
-function applyDependencyOperations(operations, deps, sourceDeps) {
+function applyDependencyOperations(operations, deps) {
   operations.add.forEach(dep => deps[dep.name] = dep.version);
   operations.remove.forEach(dep => delete deps[dep.name]);
-  operations.change.forEach(dep => {
-    let {
-      name,
-      version,
-      fromVersion
-    } = dep;
-    let sourceVersion = sourceDeps[name];
-
-    if (sourceVersion && hasSimpleHint(fromVersion)) {
-      let fromHint = getHint(fromVersion);
-      if (hasNoHint(sourceVersion) && hasNoHint(version)) {
-        version = `${fromHint}${version}`;
-      }
-      if (hasSimpleHint(sourceVersion) && hasSimpleHint(version)
-        && getHint(sourceVersion) === getHint(version)) {
-        version = version.replace(/[\^~]/, fromHint);
-      }
-    }
-
-    deps[name] = version;
-  });
+  operations.change.forEach(dep => deps[dep.name] = dep.version);
 }
 
 function mergeDependencyChanges(source, ours, theirs) {
@@ -80,11 +40,7 @@ function mergeDependencyChanges(source, ours, theirs) {
       result[dependencyKey] = {};
     }
 
-    applyDependencyOperations(
-      mergeOperations[dependencyKey],
-      result[dependencyKey],
-      source[dependencyKey]
-    );
+    applyDependencyOperations(mergeOperations[dependencyKey], result[dependencyKey]);
 
     if (!Object.keys(result[dependencyKey]).length) {
       delete result[dependencyKey];
